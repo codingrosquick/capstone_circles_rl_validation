52476c9334e8e3eadc6ac4007210aaa72a015bbe
Noé Carras <noecarras@MacBook-Pro-de-Noe.local>
2022-03-05 23:29:28 -0800

    Revert "base server for reqauiring python scripts"
    This reverts commit 4492a4dfc4107ec9a81192cb9eeaf269c53723ac.

--- server/temp_server/routes/database_exploration.route.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var DatabaseExplorationController = require('../controllers/database_exploration.controller')
-
-router.get('/', DatabaseExplorationController.getTestData)
-
-module.exports = router;
